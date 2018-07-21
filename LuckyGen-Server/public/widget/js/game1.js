0c467656f870f43d760fa1b9b8ed0d2cd4821723
Trần Duy Khánh <khanhtd@bse-corp.com>
2018-07-21 23:12:37 +0700

    Fix not win prize spin. Add indicator when waiting result from the net

--- LuckyGen-Server/public/widget/js/game1.js
+++ LuckyGen-Server/public/widget/js/game1.js
@@ -1,7 +1,14 @@
 const html = `
 <div align="center" id="lucky-spin-div-id">
-    <div class="lucky-spin-indicator" style="font-size: 20px; height: 100%;">Initializing game. Please wait...</div>
-    <table class="lucky-spin-main" border="0" cellpadding="0" cellspacing="0" style="display: none">
+    <div id="lucky-spin-indicator" style="font-size: 20px; height: 100%;">Initializing game. Please wait...</div>
+    <div id="lucky-spin-overlay" style="position: absolute;
+        width: 100%;
+        height: 100%;
+        display: none;
+        justify-content: center;
+        align-items: center;
+        font-size: 25px;">Waiting for result...</div>
+    <table id="lucky-spin-main" border="0" cellpadding="0" cellspacing="0" style="display: none">
         <tbody>
             <tr>
                 <div class="lgx-subscribe-form">
@@ -19,16 +26,16 @@ const html = `
                         <img alt="spin" disabled="" id="spin_button" onclick="playSpin();" src="https://rawgit.com/tuthanden9999/LuckyGen/master/LuckyGen/LuckySpin/image/spin_off.png">
                             <br>
                                 <br>
-                                    <!-- <div id="number">so luot quay: </div> -->
-                                    So luot quay:
+                                    <!-- <div id="number">Turns remaining: </div> -->
+                                    Turns remaining:
                                     <a id="number">
                                         ...
                                     </a>
-                                    <br>
+                                    <!-- <br>
                                         <a align="center" href="#" onclick="resetWheel(); return false;">
                                             Reset
                                         </a>
-                                    </br>
+                                    </br> -->
                                 </br>
                             </br>
                         </img>
@@ -43,7 +50,12 @@ const html = `
     </table>
 </div>
 `
-const JS_DEPENDENCIES = ["https://rawgit.com/tuthanden9999/LuckyGen/master/LuckyGen/LuckySpin/js/Winwheel.js", "http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js", "http://localhost:8080/widget/js/nebPay.js", "http://localhost:8080/widget/js/nebulas.js"]
+const JS_DEPENDENCIES = [
+    "https://rawgit.com/tuthanden9999/LuckyGen/master/LuckyGen/LuckySpin/js/Winwheel.js", 
+    "http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js", 
+    "http://localhost:8080/widget/js/nebPay.js", "http://localhost:8080/widget/js/nebulas.js"
+]
+
 const CSS_DEPENDENCIES = ['https://rawgit.com/tuthanden9999/LuckyGen/master/LuckyGen/LuckySpin/css/lucky-spin.css']
 window.wheelGame = {
     gameId: null,
@@ -52,8 +64,8 @@ window.wheelGame = {
 Object.defineProperty(window.wheelGame, 'initialized', {
     set(value) {
         if (value === true) {
-            document.querySelector('.lucky-spin-indicator').style.display = 'none'
-            document.querySelector('.lucky-spin-main').style.display = 'table'
+            document.querySelector('#lucky-spin-indicator').style.display = 'none'
+            document.querySelector('#lucky-spin-main').style.display = 'table'
         }
     },
     get() {
@@ -66,6 +78,7 @@ const GAS_LIMIT = "2000000"
 const HOST_ADDR = 'n1NjDYdhrCSKyapUriiJo2xMk1jhUC93Be5'
 var CALLBACK_URL
 const INTERVAL_TIME = 5000
+const NOT_WIN_PRIZE_TEXT = ""
 
 function initSpin({
     game_id,
@@ -100,20 +113,29 @@ function initSpin({
 }
 
 function initWheel(gameInfo) {
-    const notWinPrizeSegment = {
-        'fillStyle': randColor(),
-        'text': 'Good luck :)'
+    let totalPercentage = 0
+
+    let segments = gameInfo.prizeStructure.map(function(prize) {
+        totalPercentage += prize.prizePercentage
+        return {
+            'fillStyle': randColor(),
+            'text': prize.prize.prizeName
+        }
+    })
+
+    if (totalPercentage < 100) {
+        segments = segments.concat([{
+            'fillStyle': randColor(),
+            'text': NOT_WIN_PRIZE_TEXT
+        }])
+        window.wheelGame.notWinPrizeIndex = segments.length
     }
+
     return new Winwheel({
-        'numSegments': gameInfo.prizeStructure.length + 1, // Include 'not win' prize
+        'numSegments': segments.length, // Include 'not win' prize
         'outerRadius': 212,
         'textFontSize': 28,
-        'segments': gameInfo.prizeStructure.map(function(prize) {
-            return {
-                'fillStyle': randColor(),
-                'text': prize.prize.prizeName
-            }
-        }).concat([notWinPrizeSegment]),
+        'segments': segments,
         'animation': {
             'type': 'spinToStop',
             'duration': 5,
@@ -193,6 +215,10 @@ function playSpin() {
     var callFunction = "spin";
     var callArgs = JSON.stringify([window.wheelGame.gameId, window.wheelGame.playerId]);
     const serialNumber = nebPay.call(window.wheelGame.contractAddress, "0", callFunction, callArgs, options);
+
+    $("#lucky-spin-overlay").css('display', 'flex')
+    $("#lucky-spin-main").css('opacity', '0.3')
+
     intervalQuery = setInterval(() => {
         funcIntervalQuery(serialNumber);
     }, INTERVAL_TIME);
@@ -206,12 +232,16 @@ function funcIntervalQuery(serialNumber) {
         var respObject = JSON.parse(resp)
         console.log({respObject})
         if (respObject.data.status === 1) {
+            $("#lucky-spin-overlay").css('display', 'none')
+            $("#lucky-spin-main").css('opacity', '1')
+
             var result = JSON.parse(respObject.data.execute_result)
             if (result == "-1") {
-                startSpin(5);
+                startSpin(window.wheelGame.notWinPrizeIndex);
             } else {
                 startSpin(result);
             }
+            getPlayerById(window.wheelGame.playerId)
             clearInterval(intervalQuery);
         } else if (respObject.data.status === 0) {
             clearInterval(intervalQuery);
@@ -245,6 +275,7 @@ var wheelSpinning = false;
 
 function startSpin(prize) {
     var stopAt = ((360 / wheelGame.theWheel.numSegments) * (prize - 1)) + 1 + Math.floor((Math.random() * ((360 / wheelGame.theWheel.numSegments) - 2)));
+    console.log({stopAt})
     wheelGame.theWheel.animation.stopAngle = stopAt;
     // if (wheelSpinning == false) {
         document.getElementById('spin_button').src = "https://rawgit.com/tuthanden9999/LuckyGen/master/LuckyGen/LuckySpin/image/spin_off.png";
@@ -264,5 +295,9 @@ function resetWheel() {
 }
 
 function alertPrize(indicatedSegment) {
-    alert("You have won " + indicatedSegment.text);
+    if (indicatedSegment.text === NOT_WIN_PRIZE_TEXT) {
+        alert('Oops. You don\'t win this game. Play again?')
+    } else {
+        alert("Congratulation! You have won " + indicatedSegment.text);        
+    }
 }
